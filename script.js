5b87f69db40e2d3e96e3cb28c56061e2c8854e06
Christopher Nelson <christophernelson@Christophers-MacBook-Pro.local>
2020-02-08 13:31:16 -0500

    Now hitting enter will add an item.  I cleaned up some of the code as well

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Set Variables
 let input = document.getElementById('input');
 let ul = document.getElementsByTagName('ul')[0];
 const addButton = document.getElementById('addButton');
@@ -7,18 +8,29 @@ const removeButton = document.getElementById('removeLastItem');
 
 
 
+//Declare Functions
+function addListItem(){
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = '';
+}
 
-addButton.addEventListener('click', ()=>{
-  let li = document.createElement('li');
-  li.textContent = input.value;
-  ul.appendChild(li);
-  input.value = '';
-})
-
-removeButton.addEventListener('click',()=>{
-  let li = document.querySelector('li:last-child');
+function removeListItem(){
+    let li = document.querySelector('li:last-child');
   ul.removeChild(li);
-})
+}
+//Add functionality so that you can hit enter and it will add the item in the input to the list.
+//KEY EVENTS: Look further into events as objects and key events!
+input.addEventListener('keypress', function(e){
+    if(e.key === 'Enter'){
+        addListItem();
+    }
+});
+
+addButton.addEventListener('click', addListItem);
+
+removeButton.addEventListener('click',removeListItem);
 //Note on the li:last-child
 //The li is not a child of all of the li's
 //What this is saying to do is to select the list item that is the last child of its parent.
